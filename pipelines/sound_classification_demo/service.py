20f0ae1619551eb143296c97fb1f771f3433737e
NeethuES-Intel <neethu.elizabeth.simon@intel.com>
2023-09-11 10:50:04 -0700

    bentoml: add sound detection pipeline (#19)
    * pipelines: added sound pipeline
    
    * bentoml: containerize & makefile changes
    
    Signed-off-by: NeethuES-intel <neethu.elizabeth.simon@intel.com>
    
    * bentoml: updated ReadMe + requirements.txt
    
    Signed-off-by: NeethuES-intel <neethu.elizabeth.simon@intel.com>
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: Antonio Martinez <jose.antonio.martinez.torres@intel.com>
    
    * Update README.md
    
    Co-authored-by: Antonio Martinez <jose.antonio.martinez.torres@intel.com>
    
    * Update README.md
    
    Co-authored-by: Antonio Martinez <jose.antonio.martinez.torres@intel.com>
    
    * Update README.md -fixed typo + formatting
    
    ---------
    
    Signed-off-by: NeethuES-intel <neethu.elizabeth.simon@intel.com>
    Co-authored-by: Antonio Martinez <jose.antonio.martinez.torres@intel.com>

---
+++ pipelines/sound_classification_demo/service.py
@@ -0,0 +1,38 @@
+########################################################################
+ # Copyright (c) Intel Corporation 2023
+ # SPDX-License-Identifier: BSD-3-Clause
+########################################################################
+
+import sys
+import subprocess
+
+import numpy as np
+import bentoml
+from bentoml.io import NumpyNdarray
+from bentoml.io import Text
+import subprocess
+import json
+import sound_classification
+
+svc = bentoml.Service("sound_classification", runners=[])
+
+@svc.api(input=Text(), output=Text())
+def classify(text: str) -> str:
+
+    data = json.loads(text)
+    try:
+     print("Input message received by the bentoml service: ",data)
+   
+     mediaPath = data["MediaPath"]
+     modelPath = data["ModelPath"]
+
+     pipelineStatus = sound_classification.classify(input=mediaPath, model=modelPath, device="CPU", sample_rate=16000)
+     print("pipelineStatus:" + str(pipelineStatus))
+     
+     if pipelineStatus == True:
+      return "Success"
+     else:
+      return "Failure"
+    except Exception as e: 
+     print(str(e))
+     return "Failure"
